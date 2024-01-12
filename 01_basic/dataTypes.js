485c39e248f3d65357083480d58b8322e864fcfc
tripathi-aman007 <106093142+tripathi-aman007@users.noreply.github.com>
2024-01-12 13:33:36 +0000

    data types

---
+++ 01_basic/dataTypes.js
@@ -0,0 +1,11 @@
+// number
+// string 
+// bigint
+// boolean
+// symbol
+// null
+let itemName = null
+let itemBrand
+
+console.log(typeof itemName);  // returns object
+console.log(typeof itemBrand); // reutrns undefined
---
+++ 01_basic/variables.js
@@ -0,0 +1,11 @@
+const accountId = 14221
+let accountEmail = "aman@aman.com"
+var accountPassword = "32133" 
+accountCity = "Orai"
+let accountState
+
+accountEmail = "aman@gmail.com"
+accountPassword = "321hey"
+accountCity = "Bengaluru"
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
