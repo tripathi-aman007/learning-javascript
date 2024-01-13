f4b171cd595cef6e42b50cd8b6a13417113fd324
tripathi-aman007 <106093142+tripathi-aman007@users.noreply.github.com>
2024-01-13 12:21:59 +0000

    array part 1

---
+++ 01_basic/array.js
@@ -0,0 +1,10 @@
+const myArr = new Array(1, 2, 3, 4, 5);
+// console.log(`MyArr: ${myArr}`);
+
+// const chunkSliceArr = myArr.slice(1, 3);
+// console.log(`ChunkSliceArr: ${chunkSliceArr}`);
+// console.log(`MyArr: ${myArr}`);
+
+// const chunkSpliceArr = myArr.splice(1, 3);
+// console.log(`ChunkSpliceArr: ${chunkSpliceArr}`);
+// console.log(`MyArr: ${myArr}`);
---
+++ 01_basic/datesTime.js
@@ -0,0 +1,19 @@
+let myDate = new Date();
+
+// console.log("Printing the date obejct");
+// console.log(myDate);
+
+// console.log("Running using toString()");
+// console.log(myDate.toString());
+
+// console.log("printing date using toDateString()");
+// console.log(myDate.toDateString());
+
+// console.log("printing date using toLocaleDateString()");
+// console.log(myDate.toLocaleDateString());
+
+console.log("printing the date using toISOString()");
+console.log(myDate.toISOString());
+
+console.log("Printing the date using toJSON()");
+console.log(myDate.toJSON());
