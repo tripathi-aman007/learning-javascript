4f098baac90e0a2a2717ceffbd78491f9fc33e11
tripathi-aman007 <106093142+tripathi-aman007@users.noreply.github.com>
2024-01-13 13:34:56 +0000

    array completed

--- 01_basic/array.js
+++ 01_basic/array.js
@@ -1,4 +1,4 @@
-const myArr = new Array(1, 2, 3, 4, 5);
+// const myArr = new Array(1, 2, 3, 4, 5);
 // console.log(`MyArr: ${myArr}`);
 
 // const chunkSliceArr = myArr.slice(1, 3);
@@ -8,3 +8,35 @@ const myArr = new Array(1, 2, 3, 4, 5);
 // const chunkSpliceArr = myArr.splice(1, 3);
 // console.log(`ChunkSpliceArr: ${chunkSpliceArr}`);
 // console.log(`MyArr: ${myArr}`);
+
+
+// **************** WAYS TO MERGE ARRAYS ******************
+// const nakedBikes = ["z900", "benelli600", "rr310"];
+// const fairingBikes = ["BMWS1000rr", "ZX10r", "Aprilia457"];
+
+// const allBikes = nakedBikes.concat(fairingBikes);
+// const myAllBikes = [...nakedBikes, ...fairingBikes];
+
+// console.log("Using Concat to merge two arrays"allBikes);
+// console.log("Using Spread Operator to merege unlimited arrays: ", myAllBikes);
+
+const myBikes = [
+    "z900", "benelli600", "rr310", 
+    ["BMWS1000rr", 
+    ["ZX10r","Aprilia457"],
+    "Speed400"],
+    "Harley400"
+];
+
+// console.log(myBikes);
+// console.log(myBikes.flat(Infinity));
+
+console.log(Array.isArray("Aman"));
+console.log(Array.from("Aman"));
+
+// creating array by combining mutliple variables.
+let score1 = 100;
+let score2 = 200;
+let score3 = 300;
+
+console.log(Array.of(score1, score2, score3));
