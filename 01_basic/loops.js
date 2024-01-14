c1cced0be6ddacbcb1e51fb1bc527b2f06fd9bb4
tripathi-aman007 <106093142+tripathi-aman007@users.noreply.github.com>
2024-01-14 17:21:32 +0000

    finish part 1

---
+++ 01_basic/loops.js
@@ -0,0 +1,37 @@
+// const domains = ["Frontend", "Backend", "Data Analytics", "Android", "IOS"];
+
+// domains.forEach( function (domain) {
+//     console.log(`I love ${domain}`);
+//     }
+// )
+
+// domains.forEach( (item) => {
+//     console.log(item);
+// })
+
+// function printMe(item) {
+//     console.log(item);
+// }
+
+// domains.forEach(printMe);
+
+
+
+// const myLanguages = [
+//     {
+//         langName : "JavaScript",
+//         fileExtension : "js"
+//     },
+//     {
+//         langName : "Python",
+//         fileExtension : "py"
+//     },
+//     {
+//         langName : "C++",
+//         fileExtension : "cpp"
+//     },
+// ]
+
+// myLanguages.forEach( (item, index) => {
+//     console.log(item.langName);
+// } )
