48a3b48244f6a719897c19dcc5ed70d18e4e28fc
tripathi-aman007 <106093142+tripathi-aman007@users.noreply.github.com>
2024-01-14 09:28:58 +0000

    object part 1

---
+++ 01_basic/objects.js
@@ -0,0 +1,73 @@
+// WE CAN CREATE OBJECT IN 2 WAYS:
+
+// 1. SINGLETON OBJECTS 
+// CREATED USING CONSTRUCTOR -> OBJECT.CREATE
+
+// defining the Symbol
+const mySymbol = Symbol("key1");
+
+// 2. OBJECT LITERALS
+const userOne = {
+    userName : "Jack Ryan",
+    userEmail : "jackryan@mail.com",
+    [mySymbol] : "Symbol Value",
+    "user city" : "London",      
+    isLogged : true,
+    age : 19
+};
+
+// console.log(userOne.userEmail);
+// console.log(userOne["user city"]);     // you can't fetch this object key with (.) operator
+// console.log(userOne[mySymbol]);
+// console.log(typeof userOne[mySymbol]);
+
+// using FREEZE we are making the object immutable (can't change the value).
+// userOne.userEmail = "jack@mail.com";
+// Object.freeze(userOne);
+// userOne.userEmail = "jack@789mail.com";
+// console.log(userOne);
+
+// userOne.greeting = function() {
+//     console.log("Hello user one");
+// }
+
+// console.log(userOne.greeting);
+// userOne.greeting();
+
+
+const tinderUser = {
+    contactNumber : 8176060086,
+    email : "aman@google.com",
+    fullName : {
+        userFullName : {
+            firstName : "aman",
+            lastName : "tripathi"
+        }
+    }
+}
+
+// console.log(tinderUser.fullName.userFullName.lastName);
+const objectOne = { 1 : "one", 2 : "two" };
+const objectTwo = { 3 : "three", 4 : "four" };
+const objectThree = { 5 : "five", 6 : "six" };
+
+// COMBING TWO OBJECTS
+// const newObj = { objectOne, objectTwo };
+// const mergedObject = Object.assign( {}, objectOne, objectTwo, objectThree);
+// const mergedObject = { ...objectOne, ...objectTwo, ...objectThree };
+
+
+const newUser = {
+    contactNumber : 8176060086,
+    email : "aman@google.com",
+    name : "aman"
+};
+
+// console.log(newUser);
+// returns the array of keys & values in result
+// console.log(Object.keys(newUser)); 
+// console.log(Object.values(newUser)); 
+// returns the new object inside which it combines all the entry as a key value pair in new object.
+// console.log(Object.entries(newUser));
+
+console.log(Object.hasOwnProperty("name"));
